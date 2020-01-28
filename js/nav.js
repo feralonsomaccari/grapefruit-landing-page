e94e9f0a499794409d05c66bc53b09e2bb40faf4
feralonsomaccari <feralonsomaccari@gmail.com>
2020-01-28 20:38:36 -0300

    Add grapefruit image with arrows

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,14 @@
-const mobileNav = document.querySelector(".mobile-nav");
-const hamburger = document.querySelector(".main-nav__hamburger");
-const backdrop = document.querySelector(".backdrop");
+// const mobileNav = document.querySelector(".mobile-nav");
+// const hamburger = document.querySelector(".main-nav__hamburger");
+// const backdrop = document.querySelector(".backdrop");
 
-hamburger.addEventListener("click", () => {
-  /* classList returns DOMTokenList wich its not a array so I have to cast it to one to use the 'includes' function */
-  [...mobileNav.classList].includes("mobile-nav--show") ? mobileNav.classList.remove("mobile-nav--show") : mobileNav.classList.add("mobile-nav--show");
-  backdrop.style.display = "block";
-});
+// hamburger.addEventListener("click", () => {
+//   /* classList returns DOMTokenList wich its not a array so I have to cast it to one to use the 'includes' function */
+//   [...mobileNav.classList].includes("mobile-nav--show") ? mobileNav.classList.remove("mobile-nav--show") : mobileNav.classList.add("mobile-nav--show");
+//   backdrop.style.display = "block";
+// });
 
-backdrop.addEventListener("click", () => {
-  mobileNav.classList.remove("mobile-nav--show");
-  backdrop.style.display = "none";
-});
+// backdrop.addEventListener("click", () => {
+//   mobileNav.classList.remove("mobile-nav--show");
+//   backdrop.style.display = "none";
+// });
